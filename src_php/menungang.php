2b07afac8d8bd00ab9ee67e88f4087e2a54f3da2
Nguyễn Hoàng Huynh <nguyenhoanghuynh.vn@gmail.com>
2024-10-20 00:00:52 +0700

    Update menungang.php

--- src_php/menungang.php
+++ src_php/menungang.php
@@ -6,10 +6,13 @@
     <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=3">LIÊN HỆ</a>
     <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=4">ĐẶT XE</a>
 </div>
-<div style="margin-left:30px;">
-<form action="sanpham.php" method="post" style="display:inline-flex">
-<input type="text" name="timkiem" title="Vui lòng nhập loại xe cần tìm!" style="width:200px;height:20px">
-<input type="submit" name="timkiem__submit" value="Tìm kiếm">
-</form>
+<div style="margin-left:450px;">
+    <form action="index.php?pid=7" method="post" style="display:inline-flex">
+        <input type="search" name="timkiem" title="Vui lòng nhập loại xe cần tìm!" style="width:200px;height:25px;">
+        <input type="submit" name="timkiem__submit" value="Tìm kiếm">
+    </form>
+</div>
+<div style="margin-left:20px">
+    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=6">ĐĂNG NHẬP</a>
 </div>
 </div>
