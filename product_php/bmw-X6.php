2438f9c713968acd822a3fea76811a356a6709c2
mintuyenn <caotuyen281005@gmail.com>
2024-10-22 19:02:24 +0700

    Update bmw-X6.php

---
+++ product_php/bmw-X6.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="vi">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="main.css" />
+    <style>html {
+      scroll-behavior: smooth; /* Chuyển động cuộn mượt */
+    }</style>
+    <title>BMW X6 | BMW Việt Nam</title>
+  </head>
+  <body>
+   
+    <div id="header">
+      <div id="gioithieu">
+        <h2>THE X6</h2>
+        <h3>BMWX6</h3>
+      </div>
+      <a id="tuvan" href="./lienhe.php">Tư vấn chi tiết</a>
+      <table>
+        <tr>
+          <td>Giá từ</td>
+          <td>Nhiên liệu</td>
+        </tr>
+        <tr>
+          <td>3.799.000.000 VNĐ</td>
+          <td>Xăng</td>
+        </tr>
+      </table>
+      <div class="decoration">
+        <a id="thietke" href="#Footer">Thiết kế</a>
+        <a id="tskt" href="#TSKTT">Thông số kĩ thuật</a>
+        <a
+          id="chitiet"
+          href="https://drive.google.com/file/d/1OMilBqmhPb-IUchLbaxmdJe8sw955wF2/view?usp=sharing"
+          >Thông số kĩ thuât</a
+        >
+      </div>
+      <br />
+      <hr />
+      <p class="gt">
+        <span style="font-size: 20px; font-weight: bold"> X6 xDrive40i:</span>
+        <br />
+        Lượng nhiên liệu tiêu thụ tích hợp (l/100 km): 13,33
+        <br />
+        Lượng nhiên liệu tiêu thụ trong đô thị (l/100 km): 16.19
+        <br />
+        Lượng nhiên liệu tiêu thụ ngoài đô thị (l/100 km): 11.68 Số chứng nhận:
+        22KDN/000270
+        <br /><br />
+        <br />BMW X6 là sự kết hợp giữa ngoại hình độc đáo và tính thế thao nhờ
+        động cơ mạnh mẽ và khung gầm chắc chắn. <br /><br /><br />
+      </p>
+    </div>
+    <div id="Footer">
+      <div class="phanthietke">
+        <h1 class="thietkenoibat">THIẾT KẾ NỔI BẬT CỦA BMW X6.</h1>
+        <h2 class="thietkenoibat">Ngoại thất.</h2>
+        <div id="ngoaithat">
+          <div class="mota">
+            <img src="../phananh/thamchaomung.webp" />
+            <div class="text">
+              <h1>Thảm chào mừng.</h1>
+              <p>
+                Thảm chào mừng. Chức năng thảm chào mừng (Welcome Light
+                Carpet)tạo hiệu ứng thảm ảnh sáng, chiếu sáng hai bên cửa xung
+                quanh lối vào khi cửa mở, tạo không khí chào đón xung quanh ngay
+                tại lối vào. Thảm ánh sáng chào mừng cũng làm cho việc lên/
+                xuống xe trong không gian tối thuận tiện và an toàn hơn.
+              </p>
+            </div>
+          </div>
+          <div class="mota">
+            <img src="../phananh/cuasotroi.webp" />
+            <div class="text">
+              <h1>Cửa sổ trời Panorama.</h1>
+              <p>
+                Cửa sổ trời giúp không gian bên trong tươi mới và trong lành khi
+                mở cửa, đồng thời nó cũng tạo ra một không gian ngập tràn ánh
+                sáng nội thất khi đóng.Mở hoặc đóng chỉ bằng một chạm trên nút
+                bấm hoặc bằng chìa khóa xe, có tính năng trượt và nâng. Có đi
+                kèm với rèm che nắng và tấm ngăn gió lùa.
+              </p>
+            </div>
+          </div>
+          <div class="mota">
+            <img src="../phananh/banhmam.webp" />
+            <div class="text">
+              <h1>Bánh mâm.</h1>
+              <p>
+                Mâm xe hợp kim kích thước 20-inch, kiểu 740M Bicolour Orbit Grey
+                sử dụng lốp runflat với kích thước lốp trước là 275/45 R20 và
+                lốp sau là 305/40 R20.
+              </p>
+            </div>
+          </div>
+        </div>
+        <br /><br /><br /><br />
+        <h2 class="thietkenoibat">Nội thất.</h2>
+        <div class="noithat">
+          <div class="motaa">
+            <img src="../phananh/denviennoithat.webp" />
+            <div class="text">
+              <h1>Đèn viền nội thất.</h1>
+              <p>
+                Bao gồm đèn viền xung quanh ở cửa trước và cửa sau, tạo ra bầu
+                không khí thoải mái và ấm cúng bên trong nội thất. Đèn chào mừng
+                chiếu sáng khu vực cửa trước và cửa sau. Đèn viền nội thất có
+                các màu: Trắng, Xanh dương, Cam, Vàng đồng, Tím lilac và xanh lá
+                cây là các màu cơ bản
+              </p>
+            </div>
+          </div>
+          <div class="motaa">
+            <img src="../phananh/chitietnoithat.webp" />
+            <div class="text">
+              <h1>Các chi tiết nội thất ốp pha lê 'CraftedClarity'.</h1>
+              <p>
+                Các chi tiết nội thất ốp pha lê 'CraftedClarity'. Các chi tiết
+                pha lê 'CraftedClarity' được sử dụng trong nội thất đều được làm
+                thủ công giúp tăng tính trực quan và thẩm mỹ cho nội thất. Các
+                chi tiết được làm từ pha lê như cần số, núm điều khiển iDrive,
+                nút Start/ Stop
+              </p>
+            </div>
+          </div>
+        </div>
+        <h1 class="thietkenoibat">KHẢ NĂNG VẬN HÀNH CỦA BMW X6.</h1>
+        <p style="font-size: 20px; font-weight: 200; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin-left: 30px; margin-right: 25px;">Động cơ hiệu suất cao 8 xylanh cho công suất lên đến 530HP, kết hợp với hệ thống treo thích ứng M giúp chiếc BMW X6 trở thành một chiếc Sport Activity Coupé có khả năng di chuyển vượt trội trên mọi địa hình.</p>
+        
+        <div class="carousel">
+          <button class="arrow left" onclick="prevSlide()">&#10094;</button>
+          <div class="slides-wrapper">
+              <div class="slides">
+                  <div class="slide">
+                      <img src="../phananh/canhluotgio.webp">
+                      <div class="content">
+                          <h2>Cánh lướt gió phía sau M Carbon.</h2>
+                          <p>Cánh lướt gió phía sau M Carbon nhấn mạnh thiết kế mạnh mẽ, thể thao của xe, đồng thời giúp giảm lực cản nâng khí động học ở sau xe.</p>
+                      </div>
+                  </div>
+                  <div class="slide">
+                      <img src="../phananh/ghethethao.webp">
+                      <div class="content">
+                          <h2>Ghế thể thao cho người lái và hành khách phía trước.</h2>
+                          <p>Năng động và thoải mái: ghế thể thao tiêu chuẩn, có chức năng chỉnh điện đa hướng cho hàng ghế trước có nhiều tùy chọn điều chỉnh điện. Ngoài ra, hàng ghế trước còn có tính năng điều chỉnh đệm bưng lưng, giúp người ngồi trên xe có tư thế ngồi thoải mái và ổn định cơ thể, đặc biệt là khi vào cua gấp.</p>
+                      </div>
+                  </div>
+                  <div class="slide">
+                      <img src="../phananh/drive.webp">
+                      <div class="content">
+                          <h2>BMW xDrive.</h2>
+                          <p>Hệ thống dẫn động bốn bánh BMW xDrive thông minh có khả năng linh hoạt lực kéo giữa cầu trước và cầu sau với tốc độ xử lý cực nhanh. Hệ dẫn động xDrive giúp xe linh hoạt hơn, bám đường tốt hơn và an toàn hơn trên mọi điều kiện địa hình, có khả năng ngăn chặn các tình huống thừa lái và thiếu lái, hỗ trợ vào cua tốt hơn.</p>
+                      </div>
+                  </div>
+                  <div class="slide">
+                      <img src="../phananh/control.webp">
+                      <div class="content">
+                          <h2>Driving Experience Control.</h2>
+                          <p>Chức năng Driving Experience Control giúp người lái có thể tùy chỉnh chế độ lái theo mong muốn, bao gồm 3 chế độ: Comfort - chế độ tiêu chuẩn, ECO PRO - chế độ tiết kiệm nhiên liệu và Sport - chế độ lái xe thể thao. Chức năng Adaptive Mode sẽ điều chỉnh liên tục hệ thống treo thích ứng, hệ thống lái và hộp số để phù hợp với tình hình lái xe ngay lúc đó.</p>
+                      </div>
+                  </div>
+                  <div class="slide">
+                      <img src="../phananh/hethongtreo.webp">
+                      <div class="content">
+                          <h2>Hệ thống treo thích ứng M.</h2>
+                          <p>Hệ thống treo thích ứng M có khả năng điều chỉnh để thích ứng với điều kiện đường xá và người lái. Chủ xe cũng có thể điều chỉnh các đặc tính của hệ thống treo thông qua chức năng chuyển đổi chế độ lái (Driving Experience), chức năng này cho phép chủ xe chuyển đổi chế độ lái theo sở thích - từ sự thoải mái đến cảm giác lái thể thao với khả năng xử lý cực kỳ nhanh nhẹn.</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <button class="arrow right" onclick="nextSlide()">&#10095;</button>
+      </div>
+      <script>
+        let currentSlide = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+  
+  function showSlide(index) {
+      if (index >= totalSlides) currentSlide = 0;
+      else if (index < 0) currentSlide = totalSlides - 1;
+      else currentSlide = index;
+  
+      const offset = -currentSlide * 100;
+      document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+  }
+  
+  function nextSlide() {
+      showSlide(currentSlide + 1);
+  }
+  
+  function prevSlide() {
+      showSlide(currentSlide - 1);
+  }
+      </script>
+          <h2 class="BMWW">Original BMW Accessories</h2>
+          <div id="Original">
+            <div class="gioithieu">
+              <img src="../phananh/copnoc.webp" />
+              <div class="tieude">
+                <h1>Thanh ngang cốp nóc BMW.</h1>
+                <p>
+                  Các thanh ngang cốp nóc có thể được kết
+hợp với tất cả các khung hành lý và cốp nóc
+BMW thêm phần hoàn hảo cho ngoại hình
+toàn diện của chiếc xe. Các bộ phận này dễ
+dàng lắp đặt mà không cần dụng cụ và tuân
+thủ tiêu chuẩn chất lượng và an toàn cao
+nhất nhờ kết cấu chắc khỏe và hệ thống
+khóa chống trộm.
+                </p>
+              </div>
+            </div>
+            <div class="gioithieu">
+              <img src="../phananh/cuasotroi.webp" />
+              <div class="tieude">
+                <h1>Hộp mui trần cabin BMW 520 bằng bạc titan/màu đen.</h1>
+                <p>
+                  Hộp mui trần cabin BMW hiện đại màu đen
+với các tấm ốp bằng bạc titan bên hông có
+dung tích chứa 520 lít và tương thích với mọi
+hệ thống cốp nóc BMW. Do có thể mở từ cả
+hai bên và mỗi bên đều có khóa ba chạc ở
+giữa, tiện lợi khi chất hàng hóa và khóa
+chống trộm.
+                </p>
+              </div>
+            </div>
+            <div class="gioithieu">
+              <img src="../phananh/banhmam.webp" />
+              <div class="tieude">
+                <h1>Thảm khoang hành lý.</h1>
+                <p>Thảm chống trượt, chịu nước và bền bỉ với
+                  mép cao ở cả 4 bên giúp bảo vệ khoang
+                  hành lý khỏi bụi bẩn và ẩm ướt. Thảm màu
+                  đen thêm phần hoàn hảo cho nội thất xe với
+                  các Trang bị cơ bản.
+                  
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <h2 class="BMWW" style="font-weight: 400;">XỨNG DANH "MÃNH THÚ ĐẦU ĐÀN" Ở DÒNG XE GẦM CAO</h2>
+          <div id="Original">
+            <div class="gioithieu">
+              <img src="../phananh/đàu xe.webp" />
+              <div class="tieude">
+                <h1>Đầu xe thu hút ánh nhìn.</h1>
+                <p>
+                  Với lướt tản nhiệt kích thước lớn có thể phát sáng kết hợp cùng gói trang bị M Sport, BMW X6 mang lại cảm giác thể thao, mạnh mẽ - xứng danh mãnh thú đầu đàn.
+                </p>
+              </div>
+            </div>
+            <div class="gioithieu">
+              <img src="../phananh/kieudang.webp" />
+              <div class="tieude">
+                <h1>Kiểu dáng thể thao như xe Coupé.</h1>
+                <p>
+                  Thiếu kế nóc xe kiểu dáng coupé thời thượng. BMW X6 sở hữu mâm M 20 inch thể thao kết hợp cùng hệ thống phanh hiệu năng cao M Sport giúp mỗi chuyến hành trình cùng BMW X6 là một trải nghiệm riêng biệt.
+                </p>
+              </div>
+            </div>
+            <div class="gioithieu">
+              <img src="../phananh/menhdanh.webp" />
+              <div class="tieude">
+                <h1>Mệnh danh là "Mãnh quái".</h1>
+                <p>
+                  Mạnh mẽ và đậm chất thể thao, BMW X6 sở hữu ngoại hình đặc trưng của dòng xe gầm cao X Series và tính thể thao của một chiếc coupé. Động cơ TwinPower Turbo hiệu suất cao và hệ thống dẫn động hai cầu thông minh BMW xDrive - bảo đảm lực bám đường tuyệt vời trên mọi địa hình.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 id="TSKTT" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: black;
+          font-weight: 300;
+          margin-left: 30px;">THÔNG SỐ KĨ THUẬT</h1>
+          <h3 style="font-size: 25px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: 200; margin-left: 30px;">BMW X6 xDrive40i.</h3></div>
+          <div> <img src="../phananh/thongsokithuat.webp"> </div>
+          <div>
+          <table class="bangkithuat">
+            <tr>
+              <td>Công suất động cơ kW (hp) tại vòng/ phút </td>
+              <td class="chuthich">250kW (340HP) tại 5500-6500 </td>
+            </tr>
+            <tr>
+              <td>Vân tốc tối đa (km/h) :</td>
+              <td class="chuthich">250
+              </td>
+            </tr>
+            <tr>
+              <td>Thời gian tăng tốc từ 0-100km/h (s)</td>
+              <td class="chuthich ">5.7–5.5</td>
+            </tr>
+            <tr>
+              <td>Lượng nhiên liệu tiêu thụ kết hợp (l/100km) (theo tiêu chuẩn WLTP)</td>
+              <td class="chuthich">13.33</td>
+            </tr>
+            <tr>
+              <td>Lượng phát thải CO2 kết hợp (g/km) (WLTP):</td>
+              <td class="chuthich">206</td>
+            </tr>
+          </table>
+          <h1 class="BMWW" style="margin-top: 200px; font-weight: 400;">DỊCH VỤ VÀ HỖ TRỢ CHO BMW X6.</h1>
+          <div class="tuvan"><img style="margin-left: 30px;" src="../phananh/tuvan.webp"></div>
+          <div class="thacmac">
+            <h1 style="font-size: 25px; font-weight: 400; padding-top: 40px;">BẠN CÓ MUỐN ĐƯỢC TƯ VẤN TRỰC TIẾP?</h1>
+            <p>Nếu bạn có bất kỳ thắc mắc nào, cần thêm thông tin hoặc muốn biết các ưu đãi cụ thể về BMW X6, vui lòng liên hệ với Đại lý BMW gần nhất. Chuyên viên tư vấn BMW chuyên nghiệp của chúng tôi sẽ sẵn lòng tư vấn cá nhân cho bạn qua điện thoại hoặc trực tiếp tại showroom.​
+            </p>
+            <a href="">Liên hệ showroom gần nhất</a>
+          </div>
+          <div><p style="font-size: 20px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: 200; margin-top: 200px; margin-left: 20px;">(*) Tất cả hình ảnh và video có thể khác so với phiên bản thực tế. Liên hệ showroom gần nhất để được tư vấn giá xe và phiên bản theo nhu cầu của bạn.
+          
+         
+          </div>
+        </body >
+      </html>
