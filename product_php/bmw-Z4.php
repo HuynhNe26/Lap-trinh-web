e71a8d0200b6aa385fa82acb168cc7e328364972
duyphuu <nguyenduyphu1309@gmail.com>
2024-10-23 00:26:11 +0700

    Update bmw-Z4.php

---
+++ product_php/bmw-Z4.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BMW Z4</title>
+    <style>
+      body {
+  background-image: url("../image/stare-logo-bmw-jednak-zostaje-nowe-tylko-dla-mediw-2.jpg");
+  background-size: 100%, 100%;
+  background-repeat: no-repeat;
+}
+.container {
+  margin: 100px;
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+.container img {
+  width: 10%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 10px;
+  border: 2px solid white;
+  transition: all ease-in-out 0.5s;
+}
+.container img:hover {
+  width: 50%;
+}
+
+      
+    </style>
+  </head>
+  <div class="container">
+    <a href="./index-z4.php">
+      <img src="../image/BMW Z.avif" />
+      <img src="../image/bmw z4 xám.jpg" />
+      <img src="../image/bmw-z4 đỏ.jpg" />
+      <img src="../image/bmw z4 green.avif" />
+    </a>
+  </div>
+</html>
