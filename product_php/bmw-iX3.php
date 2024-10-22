575e67c735a38db0c3195da42061669854e3ca12
duyphuu <nguyenduyphu1309@gmail.com>
2024-10-23 00:29:16 +0700

    Update bmw-iX3.php

---
+++ product_php/bmw-iX3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BMW IX3</title>
+    <style>
+      body {
+  background-image: url("../image/stare-logo-bmw-jednak-zostaje-nowe-tylko-dla-mediw-2.jpg");
+  background-size: 100%, 100%;
+  background-repeat: no-repeat;
+}
+.container {
+  margin: 100px;
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+.container img {
+  width: 10%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 10px;
+  border: 2px solid white;
+  transition: all ease-in-out 0.5s;
+}
+.container img:hover {
+  width: 50%;
+}
+
+    </style>
+  </head>
+  <div class="container">
+    <a href="./index-ix3.php">
+      <img src="../image/bmw ix3-navy.jpg" />
+      <img src="../image/bmw ix3 black.jpg" />
+      <img src="../image/2021-bm-wi-x-32-1588037323462.webp" />
+      <img src="../image/background.jpg" />
+    </a>
+  </div>
+</html>
