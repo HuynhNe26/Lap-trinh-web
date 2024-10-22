33a8dce08051518e7e308a5049a07fb907d934ee
mintuyenn <caotuyen281005@gmail.com>
2024-10-22 18:49:37 +0700

    Update bmw-X7.php

---
+++ product_php/bmw-X7.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="main.css" />
+    <title>BMW X7| BMW VIỆT NAM</title>
+    <style>
+      html {
+        scroll-behavior: smooth; /* Chuyển động cuộn mượt */
+      }
+    </style>
+  </head>
+  <body>
+    <div id="header">
+      <div id="gioithieu">
+        <h2>THE X7</h2>
+        <h3>BMWX7</h3>
+      </div>
+      <a id="tuvan" href="./lienhe.php">Tư vấn chi tiết</a>
+      <a id="tuvan" href="">Đăng kí lái thử</a>
+      <table>
+        <tr>
+          <td>Giá từ</td>
+          <td>Nhiên liệu</td>
+        </tr>
+        <tr>
+          <td>5.549.000.000 VNĐ</td>
+          <td>Xăng</td>
+        </tr>
+      </table>
+      <div class="decoration">
+        <a id="thietke" href="#Footer">Thiết kế</a>
+        <a id="tskt" href="#tsktx7">Thông số kĩ thuật</a>
+        <a id="taichinh" href="#tc">Tài chính</a>
+        <a id="phutung" href="#tieuhaonhienlieu">Dịch vụ phụ tùng</a>
+        <a
+          id="chitiet"
+          href="https://drive.google.com/file/d/1_SteL3e1zgCwz_gqqxs1Rz9Lk9zjRcO3/view"
+          >Thông số kĩ thuât</a
+        >
+      </div>
+      <br />
+      <hr />
+      <p class="gt">
+        Được định vị là mẫu SAV (Sport Activity Vehicle) cao cấp nhất của BMW,
+        BMW X7 mới thuyết phục tất cả bằng sự thanh lịch và khả năng vượt địa
+        hình hoàn hảo. Phiên bản mới nhất của BMW X7 được BMW thay đổi toàn diện
+        từ thiết kế ngoại thất, không gian nội thất và bổ sung thêm nhiều công
+        nghệ hỗ trợ lái ấn tượng.
+      </p>
+    </div>
+    <div id="Footer">
+      <div class="phanthietke">
+        <h1 class="thietkenoibat">ĐIỂM NỔI BẬT TRONG THIẾT KẾ NGOẠI THẤT CỦA BMW X7.</h1>
+        
+        <div id="ngoaithat">
+          <div class="mota">
+            <img src="../image/dauxe.webp" />
+            <div class="text">
+              <h1>Thiết kế đầu xe.</h1>
+              <p>
+                Thay đổi lớn nhất của X7 nằm ở phần đầu xe với lưới tản nhiệt phát sáng “BMW Iconic Glow” toát lên cá tính mạnh mẽ. Các thanh lưới tản nhiệt với thiết kế thanh kép tinh xảo kết hợp với các điểm nhấn mạ chrome độc quyền tạo nên điểm nhấn đặc biệt cho phần đầu xe.
+              </p>
+            </div>
+          </div>
+          <div class="mota">
+            <img src="../image/thanxe.webp" />
+            <div class="text">
+              <h1>Thiết kế thân xe.</h1>
+              <p>
+                Xe sở hữu chiều dài cơ sở 3,105 mm với số đo dài, rộng, cao lần lượt là 5,181 x 2,000 x 1,835 mm. Tổng quan thân xe vẫn giữ được nét cơ bắp và sang trọng. Điểm nhấn ở phần thân là thiết kế cửa sổ rộng hơn giúp không gian nội thất bên trong thoáng đãng và lấy được thêm nhiều ánh sáng.
+              </p>
+            </div>
+          </div>
+          <div class="mota">
+            <img src="../image/duoixe.webp" />
+            <div class="text">
+              <h1>Thiết kế đuôi xe.</h1>
+              <p>
+                Thiết kế đuôi xe của BMW X7 toát lên vẻ mạnh mẽ và đẳng cấp với các đường nét tinh tế và hiện đại. Đuôi xe nổi bật với đèn hậu LED chữ L đặc trưng được cách tân với đồ hoạ 3D kết hợp cùng thanh Chrome kéo dài khiến đuôi xe rộng và cơ bắp hơn hẳn.
+              </p>
+            </div>
+          </div>
+        </div>
+        <h2 style="font-size: 30px;" class="thietkenoibat">ĐIỂM NỔI BẬT TRONG THIẾT KẾ NỘI THẤT CỦA BMW X7.</h2>
+        <div class="noithat">
+          <div class="motaa">
+            <img src="../image/noithat.webp" />
+            <div class="text">
+              <h1>Nội thất hiện đại và tân tiến</h1>
+              <p>
+                Công nghệ tiên tiến cùng với các trang thiết bị được kết hợp một cách thanh lịch và trang bị hài hoà trong khoang lái. Màn hình BMW Curved Display gồm màn hình trung tâm 14,9 inch cùng bảng đồng hồ 12,3 inch tích hợp trên 1 màn hình cong ôm trọn lấy khoang lái tạo nên cảm giác tinh gọn và cao cấp cho khu vực bảng điều khiển.
+              </p>
+            </div>
+          </div>
+          <div class="motaa">
+            <img src="../image/hangghesau.webp" />
+            <div class="text">
+              <h1>Các chi tiết nội thất ốp pha lê 'CraftedClarity'.</h1>
+              <p>
+                Các chi tiết nội thất ốp pha lê 'CraftedClarity'. Các chi tiết
+                pha lê 'CraftedClarity' được sử dụng trong nội thất đều được làm
+                thủ công giúp tăng tính trực quan và thẩm mỹ cho nội thất. Các
+                chi tiết được làm từ pha lê như cần số, núm điều khiển iDrive,
+                nút Start/ Stop
+              </p>
+            </div>
+          </div>
+          <div class="hinhmau">
+            <div class="hinhphu">
+              <img src="../image/phale.webp">
+              <div class="textphu">
+                <h1>Chi tiết nội thất điểm pha lê ‘CraftedClarity’.</h1>
+                <p>Nội thất của BMW X7 gây ấn tượng với việc sử dụng các vật liệu cao cấp đặc biệt. Các điểm nhấn sang trọng như bảng điều khiển iDrive ốp pha lê nhấn mạnh sự sang trọng và đẳng cấp cho New X7.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="discover">
+          <h1 class="khampha">KHÁM PHÁ ĐIỂM NỔI BẬT TRONG CÁC CÔNG NGHỆ ĐỔI MỚI CỦA BMW X7.</h1>
+          
+          <div id="congnghe">
+            <div class="noibat">
+              <img src="../image/cuasptroi.webp" />
+              <div class="text">
+                
+                <p>
+                  CỬA SỔ TRỜI PANORAMA SKY LOUNGE.
+                </p>
+              </div>
+            </div>
+            <div class="noibat">
+              <img src="../image/travel.webp" />
+              <div class="text">
+                
+                <p>
+                  HỆ THỐNG TRAVEL VÀ COMFORT.
+                </p>
+              </div>
+            </div>
+            <div class="noibat">
+              <img src="../image/cuasptroi.webp" />
+              <div class="text">
+               
+                <p>
+                  CỬA SỔ TRỜI PANORAMA SKY LOUNGE.
+              </div>
+            </div>
+          </div>
+
+          <div class="congnghecao">
+            <h1 class="congnghetantien">CÔNG NGHỆ TÂN TIẾN CỦA BMW X7.</h1>
+            
+            <div id="congnghe">
+              <div class="noibat">
+                <img src="../image/smartphone.webp" />
+                <div class="text">
+                  <h1>Tính năng kết nối điện thoại thông minh không dây</h1>
+                  <p>
+                    Với tính năng kết nối điện thoại thông minh không dây thông qua Apple CarPlay và Android Auto, chủ nhân của X7 có thể truy cập và sử dụng các ứng dụng quen thuộc của mình ngay trong xe.
+                  </p>
+                </div>
+              </div>
+              <div class="noibat">
+                <img src="../image/access.webp" />
+                <div class="text">
+                  <h1>Comfort Access.</h1>
+                  <p>
+                    Hệ thống Truy cập Tiện nghi (Comfort Access) cho phép mở tất cả các cửa xe mà không cần sử dụng chìa khóa, đồng thời trang bị thêm chìa khóa kỹ thuật số BMW (BMW Digital Key).
+                  </p>
+                </div>
+              </div>
+              <div class="noibat">
+                <img src="../image/hedieuhanh.webp" />
+                <div class="text">
+                  <h1>Hệ điều hành BMW Operating System 8.0.</h1>
+                  <p>
+                    Bộ điều khiển BMW iDrive và hệ điều hành BMW Operating System 8.0 mới giúp việc điều khiển BMW X7 trở nên trực quan và thoải mái hơn. Điều khiển trợ lý cá nhân thông minh bằng giọng nói dễ dàng hơn giúp bạn tận hưởng niềm vui lái xe một cách tuyệt đối.
+                </div>
+              </div>
+            </div>
+
+            <div class="suitable">
+              <h1 class="tienich">SỰ THOẢI MÁI TỐI ĐA TỪ BMW X7.</h1>
+              
+              <div id="congnghe">
+                <div class="noibat">
+                  <img src="../image/7cho.webp" />
+                  <div class="text">
+                    <h1>7 chỗ ngồi thoải mái cho mọi chuyến đi.</h1>
+                    <p>
+                      BMW X7 cung cấp đến 7 chỗ ngồi thoải mái, mang lại không gian rộng rãi và tiện nghi cho mọi chuyến đi.
+                    </p>
+                  </div>
+                </div>
+                <div class="noibat">
+                  <img src="../image/doxe.webp" />
+                  <div class="text">
+                    <h1>Hỗ trợ đỗ xe Parking Assistant.</h1>
+                    <p>
+                      Trợ lý đỗ xe Parking Assistant của BMW X7 hỗ trợ đỗ xe tự động và chính xác. Với các cảm biến và camera hiện đại, hệ thống giúp bạn đỗ xe dễ dàng trong mọi không gian chật hẹp, mang lại sự tiện lợi và an toàn tối đa.
+                    </p>
+                  </div>
+                </div>
+                <div class="noibat">
+                  <img src="../image/headup.webp" />
+                  <div class="text">
+                    <h1>BMW Head-Up Display.</h1>
+                    <p>
+                      BMW Head-Up Display là một công nghệ tiên tiến được tích hợp trên các dòng xe BMW, bao gồm cả BMW X7. Chức năng này cho phép hiển thị các thông tin quan trọng trực tiếp trên kính chắn gió, ngay trong tầm nhìn của người lái. Nhờ đó, người lái có thể dễ dàng theo dõi các thông số như tốc độ, hướng dẫn dẫn đường, thông tin giải trí và cảnh báo an toàn mà không cần rời mắt khỏi đường.
+                </div>
+                
+                
+              </div>
+              <div class="noithat">
+                <div class="motaa">
+                  <img src="../image/noithat.webp" />
+                  <div class="text">
+                    <h1>Cửa sổ trời Panorama Sky Lounge.</h1>
+                    <p>
+                      Cửa sổ trời Panorama Sky Lounge mang ánh sáng tự nhiên tràn ngập vào không gian nội thất trong suốt cả ngày. Vào ban đêm, hệ thống tạo bữa tiệc thị giác độc đáo với hiệu ứng ánh sáng sống động, mang lại trải nghiệm sang trọng và tinh tế cho mọi hành trình.
+                    </p>
+                  </div>
+                </div>
+                <div class="motaa">
+                  <img src="../image/hangghesau.webp" />
+                  <div class="text">
+                    <h1>Hệ thống âm thanh Bowers & Wilkins Diamond.</h1>
+                    <p>
+                      Hệ thống âm thanh Bowers & Wilkins Diamond với âm thanh vòm 3D cho công suất 1.475 W, mang lại chất âm tuyệt vời cho hành trình của bạn thêm sinh động.
+                    </p>
+                  </div>
+                </div>
+              </div> 
+            <div id="tsktx7"><h1>THÔNG SỐ KĨ THUẬT CỦA THE NEW BMW X7.</h1>
+            <h5>BMW X7 xDrive40i.</h5>
+            <img src="../image/thốngkithuat.webp" >
+          </div> 
+          <div>
+            <table style="margin-left: 730px;" class="bangkithuatx7">
+              <tr>
+                <td>Công suất động cơ kW (hp) tại vòng/ phút </td>
+                <td class="chuthichx7">280 (381)/5,200-6,250 </td>
+              </tr>
+              <tr>
+                <td>Vân tốc tối đa (km/h) :</td>
+                <td class="chuthichx7">250
+                </td>
+              </tr>
+              <tr>
+                <td>Thời gian tăng tốc từ 0-100km/h (s)</td>
+                <td class="chuthichx7 ">5.7–5.5</td>
+              </tr>
+              <tr>
+                <td>Lượng nhiên liệu tiêu thụ kết hợp (l/100km) (theo tiêu chuẩn WLTP)</td>
+                <td class="chuthichx7">13.33</td>
+              </tr>
+              <tr>
+                <td>Lượng phát thải CO2 kết hợp (g/km) (WLTP):</td>
+                <td class="chuthichx7">206</td>
+              </tr>
+            </table>
+          </div>
+          <div class="docthem"><a href="https://drive.google.com/file/d/1_SteL3e1zgCwz_gqqxs1Rz9Lk9zjRcO3/view">Learn  more</a> </div>
+          <div id="tc"><h1>CHÍNH SÁCH TÀI CHÍNH CHO BMW X7.</h1>
+            <p>BNW thiết kế các chính sách tài chính để điều chỉnh riêng để phù hợp với nhu cầu và mong muốn của bạn</p>
+            <img src="../image/taichinh.webp">
+          </div> 
+          <div id="tieuhaonhienlieu">
+            <h1>MỨC TIÊU HAO NHIÊU LIỆU VÀ MỨC PHÁT THẢI CO2</h1>
+            <h5>BMW X7 xDrive40i:</h5>
+            <ul>
+              <li>Tiêu thụ nhiên liệu kết hợp (l/100 km): 11.04 - 12.07</li>
+              <li>Khí thải CO2 kết hợp (g/km): 255.04 - 278.82</li>
+            </ul>
+            <p>Các số liệu chính thức về tiêu thụ nhiên liệu, khí thải CO2, tiêu thụ điện và phạm vi hoạt động điện được xác định theo quy trình đo lường quy định và tuân thủ Quy định (EU) 715/2007 đã được sửa đổi. Các số liệu WLTP tính đến thiết bị tùy chọn trong trường hợp các phạm vi. Đối với các xe mới được phê duyệt kể từ ngày 01.01.2021, các thông số kỹ thuật chính thức chỉ tồn tại theo WLTP. Thông tin thêm về các phương pháp đo lường NEDC và WLTP có thể được tìm thấy tại <a href="https://www.bmw.de/de/mehr-bmw/bmw-efficientdynamics/pkw-envkv.html" target="_blank"> www.bmw.de/wltp</a> .</p>
+            <p>(*) Tất cả hình ảnh và video có thể khác so với phiên bản thực tế. Liên hệ showroom gần nhất để được tư vấn giá xe và phiên bản theo nhu cầu của bạn.</p>
+          </div>
+          <div class="showroom"><a>Liên hệ showroom gần nhất</a></div>   
+        </div>
+    </div>
+  </body>
+</html>
