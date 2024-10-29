f34ec560d2233865a6e7275c86375b85f7d352f7
Nguyễn Hoàng Huynh <nguyenhoanghuynh.vn@gmail.com>
2024-10-29 10:56:53 +0700

    Update menungang.php

--- src_php/menungang.php
+++ src_php/menungang.php
@@ -1,34 +1,35 @@
-<div style="display:inline-flex">
-<div>
-    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid">TRANG CHỦ</a>
-    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=1&matl=1&machitiettl=1&masp=1">SẢN PHẨM</a>
-    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=2">ĐĂNG KÍ LÁI THỬ</a>
-    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=3">LIÊN HỆ</a>
-    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=4">ĐẶT XE</a>
-</div>
-<div style="margin-left:450px;">
-    <form action="index.php?pid=7" method="post" style="display:inline-flex">
-        <select name="timkiem" type="search" style="width:200px;height:25px" required>
-            <option name="timkiem" type="search" value="">Vui lòng chọn</option>
-            <option name="timkiem" type="search" value="BMW 3">BMW 3</option>
-            <option name="timkiem" type="search" value="BMW 4 Convertible">BMW 4 Convertible</option>
-            <option name="timkiem" type="search" value="BMW 4 Gran Coupé">BMW 4 Gran Coupé</option>
-            <option name="timkiem" type="search" value="BMW 5">BMW 5</option>
-            <option name="timkiem" type="search" value="BMW 7">BMW 7</option>
-            <option name="timkiem" type="search" value="BMW 8 Gran Coupé">BMW 8 Gran Coupé</option>
-            <option name="timkiem" type="search" value="BMW X3">BMW X3</option>
-            <option name="timkiem" type="search" value="BMW X5">BMW X5</option>
-            <option name="timkiem" type="search" value="BMW X6">BMW X6</option>
-            <option name="timkiem" type="search" value="BMW X7">BMW X7</option>
-            <option name="timkiem" type="search" value="BMW XM">BMW XM</option>
-            <option name="timkiem" type="search" value="BMW iX3">BMW iX3</option>
-            <option name="timkiem" type="search" value="BMW i4">BMW i4</option>
-            <option name="timkiem" type="search" value="BMW i7">BMW i7</option>
-        </select> <br>
-        <input type="submit" name="timkiem__submit" value="Tìm kiếm">
-    </form>
-</div>
-<div style="margin-left:20px">
-    <a style="text-decoration:none; color:black; font-size:15px; margin-left:20px; font-weight:600;" href="index.php?pid=6&&page=dangnhap">ĐĂNG NHẬP</a>
-</div>
+<?php
+$link = new mysqli ('localhost','root','','bmw_web');
+$sql_timkiem = "SELECT * FROM sanpham";
+$result_timkiem = $link->query($sql_timkiem);
+?>
+<div style="display:inline-flex; transition:all .3s">
+    <div style="margin-top:10px">
+        <a class="menungang__a" href="index.php?pid">TRANG CHỦ</a>
+        <a class="menungang__a" href="index.php?pid=1&matl=1&machitiettl=1&masp=1">SẢN PHẨM</a>
+        <a class="menungang__a" href="index.php?pid=2">ĐĂNG KÍ LÁI THỬ</a>
+        <a class="menungang__a" href="index.php?pid=3">LIÊN HỆ</a>
+        <a class="menungang__a" href="index.php?pid=4">ĐẶT XE</a>
+    </div>
+    <div style="margin-left:350px;margin-top:5px">
+        <form action="index.php?pid=7" method="post" style="display:inline-flex">
+            <select name="timkiem" type="search" class="menungang__select" required>
+                <option name="timkiem" type="search" value="">Vui lòng chọn loại xe cần tìm!</option>
+                <?php
+                while ($row = $result_timkiem ->fetch_assoc()) {
+                ?>    
+                <option name="timkiem" type="search" value="<?php echo $row['tensp'] ?>"><?php echo $row['tensp'] ?></option>
+                <?php
+                }
+                ?>
+            </select> <br>
+            <input class="menungang__input" type="submit" name="timkiem__submit" value="Tìm kiếm">
+        </form>
+    </div>
+    <div style="margin-left:20px;">
+        <a class="menungang__a" href="index.php?pid=8"><?php echo '<img src="image/shopping-cart_icon.png" style="width:30px; height:30px">' ?></a>
+    </div>
+    <div style="margin-left:10px;margin-top:10px">
+        <a class="menungang__a" href="index.php?pid=6&&page=dangnhap">ĐĂNG NHẬP</a>
+    </div>
 </div>
