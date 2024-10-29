c7a3da92997b32c92f745052a2431fa4815dd06e
Nguyễn Hoàng Huynh <nguyenhoanghuynh.vn@gmail.com>
2024-10-29 10:52:00 +0700

    Create menudoc.php

---
+++ admin/src-quanli_php/menudoc.php
@@ -0,0 +1,33 @@
+<div class="navbar">
+        <ul>
+            <li style="margin-left:10px">
+                <a class="navbar-img" href="admin.php?pid">
+                    <?php echo ('<img class="navbar__img" src="../image/Logo.png">') ?>
+                </a>
+            </li>
+            <li class="title">Hệ Thống Quản Lý</li>
+
+            <!-- Menu Items -->
+            <li>
+                <a class="navbar__a" href="admin.php">Thống kê</a>
+            </li>
+            <li>
+                <a class="navbar__a" href="admin.php?pid=1">Quản lý đơn hàng</a>
+            </li>
+            <li>
+                <a class="navbar__a" href="admin.php?pid=2">Thống kê lượt mua</a>
+            </li>
+            <li>
+                <a class="navbar__a" href="admin.php?pid=3">Quản lý sản phẩm</a>
+            </li>
+            <li>
+                <a class="navbar__a" href="admin.php?pid=4">Thêm sản phẩm</a>
+            </li>
+            <li>
+                <a class="navbar__a" href="admin.php?pid=5">Quản lý người dùng</a>
+            </li>
+            <li>
+                <a class="navbar__a" href="logout.php">Đăng xuất</a>
+            </li>
+        </ul>
+</div>
