a9033f071032bfc50e74bbcb95ede3b7af377645
Nguyễn Hoàng Huynh <nguyenhoanghuynh.vn@gmail.com>
2024-10-30 16:15:28 +0700

    Update xoasp.php

--- admin/src-quanli_php/xoasp.php
+++ admin/src-quanli_php/xoasp.php
@@ -1,4 +1,11 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (isset($_POST['delete'])) {
     $id = $_POST['id'];
     $conn = mysqli_connect("localhost", "root", "", "bmw_web");
@@ -9,13 +16,14 @@ if (isset($_POST['delete'])) {
     $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $id);
     if ($stmt->execute()) {
-        echo "Sản phẩm đã được xóa thành công.";
+        session_start();
+        $_SESSION['message'] = "Sản phẩm đã được xóa thành công.";
     } else {
         echo "Lỗi: " . $stmt->error;
     }
     $stmt->close();
     mysqli_close($conn);
-    header("Location: index.php");
+    header("Location: admin.php?pid=3");
     exit();
 }
 ?>
