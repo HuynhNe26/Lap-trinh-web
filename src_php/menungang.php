ca9a9967be4992e4dd2e39ba4c308b6e3bb56efd
Kiettranne <164780985+Kiettranne@users.noreply.github.com>
2024-10-31 01:05:33 +0700

    Update menungang.php

--- src_php/menungang.php
+++ src_php/menungang.php
@@ -1,35 +1,46 @@
 <?php
-$link = new mysqli ('localhost','root','','bmw_web');
+$link = new mysqli('localhost', 'root', '', 'bmw_web');
+if ($link->connect_error) {
+    die("Kết nối thất bại: " . $link->connect_error);
+}
+
 $sql_timkiem = "SELECT * FROM sanpham";
 $result_timkiem = $link->query($sql_timkiem);
+if (!$result_timkiem) {
+    die("Lỗi truy vấn: " . $link->error);
+}
 ?>
-<div style="display:inline-flex; transition:all .3s">
-    <div style="margin-top:10px">
+
+<div style="display: flex; align-items: center; justify-content: flex-start; flex-wrap: nowrap; transition: all .3s;">
+    <div style="margin-top: 10px">
         <a class="menungang__a" href="index.php?pid">TRANG CHỦ</a>
         <a class="menungang__a" href="index.php?pid=1&matl=1&machitiettl=1&masp=1">SẢN PHẨM</a>
         <a class="menungang__a" href="index.php?pid=2">ĐĂNG KÍ LÁI THỬ</a>
         <a class="menungang__a" href="index.php?pid=3">LIÊN HỆ</a>
         <a class="menungang__a" href="index.php?pid=4">ĐẶT XE</a>
     </div>
-    <div style="margin-left:350px;margin-top:5px">
-        <form action="index.php?pid=7" method="post" style="display:inline-flex">
-            <select name="timkiem" type="search" class="menungang__select" required>
-                <option name="timkiem" type="search" value="">Vui lòng chọn loại xe cần tìm!</option>
-                <?php
-                while ($row = $result_timkiem ->fetch_assoc()) {
-                ?>    
-                <option name="timkiem" type="search" value="<?php echo $row['tensp'] ?>"><?php echo $row['tensp'] ?></option>
-                <?php
-                }
-                ?>
-            </select> <br>
+    <div style="margin-left: 50px; margin-top: 5px; flex-grow: 1;">
+        <form action="index.php?pid=7" method="post" style="display: inline-flex; width: 100%;">
+            <select name="timkiem" class="menungang__select" required>
+                <option value="">Vui lòng chọn loại xe cần tìm!</option>
+                <?php while ($row = $result_timkiem->fetch_assoc()): ?>
+                    <option value="<?php echo htmlspecialchars($row['tensp']); ?>"><?php echo htmlspecialchars($row['tensp']); ?></option>
+                <?php endwhile; ?>
+            </select>
             <input class="menungang__input" type="submit" name="timkiem__submit" value="Tìm kiếm">
         </form>
     </div>
-    <div style="margin-left:20px;">
-        <a class="menungang__a" href="index.php?pid=8"><?php echo '<img src="image/shopping-cart_icon.png" style="width:30px; height:30px">' ?></a>
+    <div style="margin-left: 20px;">
+        <a class="menungang__a" href="index.php?pid=8">
+            <img src="image/shopping-cart_icon.png" style="width:30px; height:30px">
+        </a>
     </div>
-    <div style="margin-left:10px;margin-top:10px">
-        <a class="menungang__a" href="index.php?pid=6&&page=dangnhap">ĐĂNG NHẬP</a>
+    <div style="margin-left: 10px; margin-top: 10px">
+        <?php if (isset($_SESSION['username'])): ?>
+    <span>Xin chào, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
+    <a class="menungang__a" href="src_php/login/dangxuat.php">Đăng xuất</a>
+<?php else: ?>
+    <a class="menungang__a" href="index.php?pid=6&page=dangnhap">ĐĂNG NHẬP</a>
+<?php endif; ?>
     </div>
 </div>
