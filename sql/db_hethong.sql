c2b414f203ae799493ebeef91ef366c1a437618a
Nguyễn Hoàng Huynh <nguyenhoanghuynh.vn@gmail.com>
2024-10-30 13:53:54 +0700

    Create db_hethong.sql

---
+++ sql/db_hethong.sql
@@ -0,0 +1,17 @@
+CREATE TABLE memember_admin (
+    tendangnhap VARCHAR(255) NOT NULL,
+    matkhau VARCHAR(255) NOT NULL,
+    hovaten VARCHAR(255) NOT NULL,
+    quyen INT,
+    PRIMARY KEY (tendangnhap)
+);
+
+INSERT INTO memember_admin (tendangnhap, hovaten, matkhau, quyen) 
+VALUES 
+('hoanghuynh','Hoàng Huynh','1234',1),
+('baouyen','Bảo Uyên','1234',1),
+('minhvu','Minh Vũ','1234',1),
+('anhkiet','Anh Kiệt','1234',1),
+('minhtuyen','Minh Tuyền','1234',1),
+('duyphu','Duy Phú','1234',1),
+('longvu','Long Vũ','1234',1);
