fcc1119f6334a013d78befe5e0c340091b1a94b2
mintuyenn <caotuyen281005@gmail.com>
2024-11-15 18:26:36 +0700

    Update thongbao.php

--- src_php/thongbao.php
+++ src_php/thongbao.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thông Báo</title>
+</head>
+<body style="text-align: center;">
+    <h1 style="color: red; margin-left:40px;margin-right:40px;">Đơn hàng đã được ghi nhận lại. Shop sẽ sớm liên hệ với quý khách sớm nhất. Cảm ơn quý khách đã tin tưởng và ủng hộ!!!</h1>
+    <form action="index.php?pid=16" method="POST">
+            <div >
+                <button style="text-decoration: none; padding:10px 40px; text-align:center;background-color: blue; font-size:30px; color:aliceblue" type="submit" name="place_order"> Xem đơn hàng và in bill</button>
+            </div>
+    </form>
+</body>
+</html>
