6cd355425e7b998322a436277175f2b658dca4f8
mintuyenn <caotuyen281005@gmail.com>
2024-11-19 18:15:47 +0700

    Update xulithanhtoan.php

--- src_php/xulithanhtoan.php
+++ src_php/xulithanhtoan.php
@@ -1,11 +1,17 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "bmw_chung");
-if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
+
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php"); 
+    exit();
 }
 
-session_start(); 
+$link_khachhang = new mysqli("localhost", "root", "", "bmw_khachhang");
+if ($link_khachhang->connect_error) {
+    die("Kết nối thất bại: " . $link_khachhang->connect_error);
+}
+
 $user_id = $_SESSION['user_id'];
 $username = $_SESSION['username'];
 $email = $_SESSION['email'];
@@ -13,10 +19,15 @@ $address = $_SESSION['address'];
 $phone = $_SESSION['phone'];
 
 
+$link_sanpham = new mysqli("localhost", "root", "", "bmw_sanpham");
+if ($link_sanpham->connect_error) {
+    die("Kết nối thất bại: " . $link_sanpham->connect_error);
+}
+
 $product_ids = array_keys($_SESSION['cart']);
 $product_ids_string = implode(',', $product_ids);
 $sql = "SELECT masp, tensp, dongia FROM sanpham WHERE masp IN ($product_ids_string)";
-$result = $conn->query($sql);
+$result = $link_sanpham->query($sql);
 
 $product_details = [];
 $total_price = 0;
@@ -47,27 +58,37 @@ $order_info = [
     'product_details' => $product_details
 ];
 
+
+
+$link_giohang = new mysqli("localhost", "root", "", "bmw_giohang");
+if ($link_giohang->connect_error) {
+    die("Kết nối thất bại: " . $link_giohang->connect_error);
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['place_order'])) {
     
-    $stmt = $conn->prepare("INSERT INTO don_hang (makhachhang, hovaten, email, diachi, sodienthoai, tongtien, ngaydat) VALUES (?, ?, ?, ?, ?, ?, NOW())");
+    $stmt = $link_giohang->prepare("INSERT INTO don_hang (makhachhang, hovaten, email, diachi, sodienthoai, tongtien, ngaydat) VALUES (?, ?, ?, ?, ?, ?, NOW())");
     $stmt->bind_param("issssi", $user_id, $username, $email, $address, $phone, $total_price);
     $stmt->execute();
     $order_id = $stmt->insert_id; 
-   
+
+    
     foreach ($product_details as $product) {
-        $stmt = $conn->prepare("INSERT INTO chitietsanpham (don_hang_id, masp, soluong, dongia) VALUES (?, ?, ?, ?)");
+        $stmt = $link_giohang->prepare("INSERT INTO chitietsanpham (don_hang_id, masp, soluong, dongia) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("iiid", $order_id, $product['masp'], $product['quantity'], $product['dongia']);
         $stmt->execute();
     }
 
-   
+    
     unset($_SESSION['cart']);
 
-    
+   
     echo "<script>window.location.href = 'index.php?pid=15';</script>";
     exit();
 }
 
-$conn->close();
+$link_giohang->close();
+$link_khachhang->close();
+$link_sanpham->close();
 ?>
-
